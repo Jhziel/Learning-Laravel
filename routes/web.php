df576affca42a46d0915bed36af4fd95d94c1a0c
Jhziel <barnedodonjaziel57@gmail.com>
2024-09-02 17:37:40 +0800

    Learning CSRF Token

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,30 @@ Route::get('/jobs', function () {
     /* $jobs = Job::with('employer')->get(); */
 
     //getting using Pagination
-    $jobs=Job::with('employer')->paginate(3);
-    return view('jobs', ['jobs' => $jobs]);
+    $jobs = Job::with('employer')->latest()->paginate(3);
+    return view('jobs.index', ['jobs' => $jobs]);
 });
+
+Route::get('/jobs/create', function () {
+
+    return view('jobs.create');
+});
+
 Route::get('/jobs/{id}', function ($id) {
 
     $job = Job::find($id);
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
+});
+
+Route::post('/jobs', function () {
+
+    Job::create([
+        'title' => request('title'),
+        'salary' => $_POST['salary'],
+        'employer_id' => 1
+    ]);
+
+    return redirect('/jobs');
 });
 Route::get('/contact', function () {
     return view('contact');
