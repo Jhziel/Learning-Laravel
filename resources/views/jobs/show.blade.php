bde8bd18486dff61cc19f89db7fa23dfc5f846b2
Jhziel <barnedodonjaziel57@gmail.com>
2024-09-05 00:37:42 +0800

    Learn how to Authorize in Routes

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -3,13 +3,14 @@
         Jobs
     </x-slot:heading>
     <ul class="mb-3">
-
+       
         <li class="mt-3">
             <p class="font-bold underline">Job Title:{{ $job->title }}</p>
             <p>Salary:{{ $job['salary'] }}</p>
+
         </li>
     </ul>
-    @can('edit-job', $job)
-        <a href="/jobs/{{ $job->id }}/edit" class="bg-blue-500 py-2 px-4 text-slate-200">Edit</a>
-    @endcan
+    {{--  @can('edit-job', $job) --}}
+    <a href="/jobs/{{ $job->id }}/edit" class="bg-blue-500 py-2 px-4 text-slate-200">Edit</a>
+    {{-- @endcan --}}
 </x-layout>
