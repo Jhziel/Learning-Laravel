c6c1491c29d493a95898c26ba0fcc7345c9a8b27
Jhziel <barnedodonjaziel57@gmail.com>
2024-09-05 00:00:53 +0800

    Not got it so well will comeback to this Lesson

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -9,6 +9,7 @@
             <p>Salary:{{ $job['salary'] }}</p>
         </li>
     </ul>
-
-    <a href="/jobs/{{ $job->id }}/edit" class="bg-blue-500 py-2 px-4 text-slate-200">Edit</a>
+    @can('edit-job', $job)
+        <a href="/jobs/{{ $job->id }}/edit" class="bg-blue-500 py-2 px-4 text-slate-200">Edit</a>
+    @endcan
 </x-layout>
